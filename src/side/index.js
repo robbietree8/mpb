527b603f7776cf3b19f49af5106f3f2b0cc94273
zhongl <zhong.lunfu@gmail.com>
2024-07-11 18:59:55 +1000

    Stash.

--- src/side/index.js
+++ src/side/index.js
@@ -1,44 +1,57 @@
-// import Plotly from 'plotly.js-dist-min'
+import Plotly from 'plotly.js-dist-min'
 
-// var values = [
-//     ['Salaries', 'Office', 'Merchandise', 'Legal', '<b>TOTAL</b>'],
-//     [1200000, 20000, 80000, 2000, 12120000],
-//     [1300000, 20000, 70000, 2000, 130902000],
-//     [1300000, 20000, 120000, 2000, 131222000],
-//     [1400000, 20000, 90000, 2000, 14102000]]
+chrome.storage.session.get(null, ({ activities: a }) => refresh(a));
+chrome.storage.session.onChanged.addListener(({ activities: a }) => refresh(a.newValue));
 
-// var data = [{
-//     type: 'table',
-//     header: {
-//         values: [["<b>EXPENSES</b>"], ["<b>Q1</b>"],
-//         ["<b>Q2</b>"], ["<b>Q3</b>"], ["<b>Q4</b>"]],
-//         align: "center",
-//         line: { width: 1, color: 'black' },
-//         fill: { color: "grey" },
-//         font: { family: "Arial", size: 12, color: "white" }
-//     },
-//     cells: {
-//         values: values,
-//         align: "center",
-//         line: { color: "black", width: 1 },
-//         font: { family: "Arial", size: 11, color: ["black"] }
-//     }
-// }]
+function refresh(activities) {
+    const pace = v => {
+        if (!v) return "--";
+        const sec = Math.round(1000 / v);
+        const d = new Date(new Date().toLocaleDateString()).setSeconds(sec);
+        return new Date(d).toLocaleTimeString();
+    }
+    const format = v => `${v.getFullYear()}-${String(v.getMonth() + 1).padStart(2, '0')}-${String(v.getDate()).padStart(2, '0')}`;
 
-// Plotly.newPlot("dt", data);
+    const values = [
+        ({ startTimeLocal: v }) => format(new Date(v)),
+        ({ averageHR: v }) => v,
+        ({ averageSpeed: v }) => pace(v),
+        ({ avgGradeAdjustedSpeed: v }) => pace(v),
+        ({ averageSpeed: s, avgGradeAdjustedSpeed: a }) => ((a ? a : s) * 60).toFixed(2),
+        ({ averageSpeed: s, avgGradeAdjustedSpeed: a, averageHR: h }) => (((a ? a : s) * 60) / h).toFixed(3),
+    ].map(f => activities.map(f));
 
-const container = document.querySelector("#dt");
+    const dataTable = {
+        type: 'table',
+        header: {
+            values: [["<b>DATE</b>"], ["<b>HR</b>"], ["<b>Pace</b>"], ["<b>GAP</b>"], ["<b>Speed.mpm</b>"], ["<b>Pref.mpb</b>"]],
+            align: "center",
+            line: { width: 1, color: 'black' },
+            fill: { color: "grey" },
+            font: { family: "Arial", size: 12, color: "white" }
+        },
+        cells: {
+            values: values,
+            align: "center",
+            line: { color: "black", width: 1 },
+            font: { family: "Arial", size: 11, color: ["black"] }
+        }
+    }
 
-function element(value) {
-    const e = document.createElement("pre");
-    e.innerText = JSON.stringify(value, null, 2);
-    return e;
+    const config = { responsive: true };
+    Plotly.newPlot("dt", [dataTable], { title: "Perf of Running" }, config);
+
+    const perfTrace = {
+        x: values[0].toReversed(),
+        y: values[5].toReversed(),
+        mode: 'lines+markers',
+        type: "scatter"
+    };
+    Plotly.newPlot("pt", [perfTrace], { title: "Perf Trace" }, config);
 }
 
-chrome.storage.session.get(null, v => {
-    container.appendChild(element(v));
-});
-chrome.storage.session.onChanged.addListener((changes, area) => {
-    container.appendChild(element(changes));
-});
-
+function debug(data) {
+    const e = document.createElement("pre");
+    e.innerText = JSON.stringify(data, null, 2);
+    document.querySelector("#dt").appendChild(e);
+}
